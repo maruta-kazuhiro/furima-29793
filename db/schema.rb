67eff77ca0faf98dc122cbb73dc1a278587355b0
maruta-kazuhiro <70316338+maruta-kazuhiro@users.noreply.github.com>
2020-09-16 12:18:33 +0900

    ユーザー管理機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 2020_09_15_015339) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", null: false
-    t.string "password", null: false
     t.string "family_name", null: false
     t.string "first_name", null: false
     t.string "family_name_kana", null: false
