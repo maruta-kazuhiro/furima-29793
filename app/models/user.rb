b06bf31de9ada8822bb067673361997f54a355c6
maruta-kazuhiro <70316338+maruta-kazuhiro@users.noreply.github.com>
2020-09-18 19:15:40 +0900

    ユーザー管理機能　修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-  validates :nickname
-  validates :family_name,  format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-  validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
-  validates :first_name_kana , format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
-  validates :birthday
+    VALID_KANJI = /\A[ぁ-んァ-ン一-龥]/
+    VALID_KATAKANA = /\A[ァ-ヶー－]+\z/
+    validates :nickname
+    validates :family_name, format: { with: VALID_KANJI, message: 'is invalid. Input full-width characters.' }
+    validates :first_name, format: { with: VALID_KANJI, message: 'is invalid. Input full-width characters.' }
+    validates :family_name_kana, format: { with: VALID_KATAKANA, message: 'is invalid. Input full-width katakana characters.'}
+    validates :first_name_kana, format: { with: VALID_KATAKANA, message: 'is invalid. Input full-width katakana characters.'}
+    validates :birthday
   end
-  validates :email, uniqueness: { case_sensitive: true },  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/, message: "is invalid. Include @." }
-  validates :password, length: { minimum: 6 }, format: { with: /\A[a-z0-9]+\z/,  message: "is invalid. Minimum 6."}
-  
+  validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/, message: 'is invalid. Minimum 6.'}
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number:3)}
-    email                 {Faker::Internet.free_email}
-    password              {"abc123"}
-    password_confirmation {password}
-    family_name           {"亜"}
-    first_name            {"亜"}
-    family_name_kana      {"ア"}
-    first_name_kana       {"ア"}
-    birthday              {Faker::Date.between(from: '1900-01-01', to: '2099-12-31')}
+    nickname              { Faker::Name.initials(number: 3) }
+    email                 { Faker::Internet.free_email }
+    password              { 'abc123' }
+    password_confirmation { password }
+    family_name           { '亜' }
+    first_name            { '亜' }
+    family_name_kana      { 'ア' }
+    first_name_kana       { 'ア' }
+    birthday              { Faker::Date.between(from: '1900-01-01', to: '2099-12-31') }
   end
 end
