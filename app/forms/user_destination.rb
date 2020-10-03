585ef50034b3614be8bf378eea84dc9e91b8e9da
maruta-kazuhiro <70316338+maruta-kazuhiro@users.noreply.github.com>
2020-10-03 12:46:42 +0900

    郵便番号の修正

--- app/forms/user_destination.rb
+++ app/forms/user_destination.rb
@@ -3,7 +3,7 @@ class UserDestination
   attr_accessor :postal_code, :shipping_from_id, :municipalities, :address, :building, :telephone_number, :item_id, :user_id, :token
 
   with_options presence: true do
-    validates :postal_code, format: { with: /\A\d{3}\-?\d{4}\z/, message: 'is invalid. Input postal code' }
+    validates :postal_code, format: { with: /\A\d{3}\-\d{4}\z/, message: 'is invalid. Input postal code' }
     validates :shipping_from_id, numericality: { other_than: 1, message: 'is invalid. You must select' }
     validates :municipalities
     validates :address
