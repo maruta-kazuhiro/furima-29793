57782942da4fe54c3711d9fa33f1ce7a8cf4b2d7
maruta-kazuhiro <70316338+maruta-kazuhiro@users.noreply.github.com>
2020-10-16 17:18:05 +0900

    プレビュー機能実装

---
+++ app/javascript/preview.js
@@ -0,0 +1,31 @@
+function add_item_image(){
+  const ImageList = document.getElementById('item-image-list');
+
+  const createImageHTML = (blob) => {
+   const imageElement = document.createElement('div');
+
+   const blobImage = document.createElement('img');
+   blobImage.setAttribute('src', blob);
+
+   imageElement.appendChild(blobImage);
+   ImageList.appendChild(imageElement);
+ };
+
+  document.getElementById('item-image').addEventListener('change', function(e){
+    const imageContent = document.querySelector('img');
+    if (imageContent){
+      imageContent.remove();
+    }
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+
+    createImageHTML(blob);
+  });
+};
+
+
+window.addEventListener("load", function(){ 
+  var path = location.pathname;
+  if (path == "/items/new")
+  { add_item_image() }
+});
